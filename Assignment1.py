a52f62307a43d1b9b5866f00e8cc3d98f1f82d0d
rahul21-10 <55239106+rahul21-10@users.noreply.github.com>
2020-05-05 18:24:54 +0530

    Update Assignment1.py

--- Assignment1.py
+++ Assignment1.py
@@ -1,8 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
+#numbers which are divisible by 7 but are not a multiple of 5, between 2000 and 3200 (both included)
 
 a= list (range(2000,3201))
 b=[] 
@@ -12,7 +8,7 @@ for i in a:
 print(b)
 
 
-# In[2]:
+# Write a Python program to find the volume of a sphere with diameter 12 cm
 
 
 d=12
@@ -20,7 +16,7 @@ v= 1.33*3.14*(d/2)*(d/2)*(d/2)
 print (v)
 
 
-# In[3]:
+# Create the below pattern using nested for loop in Python.
 
 
 for i in range(6):
@@ -33,16 +29,18 @@ for i in range(6):
     print()
 
 
-# In[6]:
+ # Write a Python program to reverse a word after accepting the input from the user
+ # Input word: AcadGild
 
 
-a = input ("Enter your First and Last Name")
+a = input ("Input Word")
 b = len(a)
 c= a[b::-1]
 print(c)
 
 
-# In[7]:
+# Write a Python program to accept the user's first and last name and then getting them 
+#printed in the the reverse order with a space between first name and last name.
 
 
 a = input ("Enter your First and Last Name")
@@ -51,20 +49,14 @@ c=a[b::-1]
 print(c)
 
 
-# In[12]:
+# Write a program which accepts a sequence of comma-separated numbers from console and
+#generate a list.
 
 
 a = input("Enter Numbers seperated by Comma").split(',')
 print(a)
 
 
-# In[ ]:
-
-
-
-
-
-# In[ ]:
 
 
 
